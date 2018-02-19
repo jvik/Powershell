276af81d251cb202f72fc9c4120548b32984cccb
Jørgen <mail@jvik.no>
2018-02-19 19:59:58 +0100

    Fixed pattern matching

--- Reset-Profiles.ps1
+++ Reset-Profiles.ps1
@@ -18,17 +18,24 @@
 #>
 
 $profile_list = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList'
+$registryarray = @()
 
 foreach ($user_item in $profile_list) {
      $user_list = Get-ChildItem -Path $user_item
      foreach ($registry_user_path in $user_list) {
         # Path is returned with HKEY_LOCAL_MACHINE prefix, while the powershell registry-cmdlets takes in HKLM:-prefix. 
-        # Line below fixes problem.
+        # Line below fixes this.
         $registry_user_path = $registry_user_path -replace 'HKEY_LOCAL_MACHINE', 'HKLM:'
         $user_profile_path = Get-ItemPropertyValue -Path $registry_user_path -Name ProfileImagePath
-        Remove-Item -Path $registry_user_path -recurse -Force | Out-file c:\temp\deleteprofiles.txt -Append
-        Remove-Item -Path $user_profile_path -recurse -Force | Out-file c:\temp\deleteprofiles.txt -Append
-        Write-Host Removed: $registry_user_path
-        Write-host Removed: $user_profile_path
+        $registryarray += New-Object psobject -Property @{'RegistryPathName' = $registry_user_path; 'ProfilePath' = $user_profile_path}
      }    
+}
+
+foreach ($profile in $registryarray) {
+  # Regex demands 3 digits in username to recreate profile. Use pattern of own choice.
+  if ($profile.ProfilePath -match "\d{3}") {
+      Remove-Item -Path $profile.RegistryPathName -recurse -Force | Out-file c:\temp\deleteprofiles.txt -Append
+      Remove-Item -Path $profile.ProfilePath -recurse -Force | Out-file c:\temp\deleteprofiles.txt -Append
+      Write-Host Sletter: $profile.ProfilePath 
+    }
 }
