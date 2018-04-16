a06329d016203fb280c0fb7164d07452c01d390a
Jørgen Vik <mail@jvik.no>
2018-04-16 14:28:02 +0200

    Add logic

--- Copy-Templates.ps1
+++ Copy-Templates.ps1
@@ -1,5 +1,9 @@
 $templateCentralStore = \\FJORD1PDC01\system$\office-templates\keolis
-$profileLoc = c:\Users\%username%\Documents\Templates
+$profileLoc = c:\Users\$env:UserName\Documents\Office-Templates
 
-robocopy $templateCentralStore $profileLoc /s /r:1 /w:1
+$directoryInfo = Get-ChildItem $profileLoc | Measure-Object
 
+if ($directoryInfo.Count -eq 0)
+{
+    robocopy $templateCentralStore $profileLoc /s /r:1 /w:1
+}
