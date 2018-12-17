01498af34acb82308eaaadf37be06732b05eef1c
Jorgen V <mail@jvik.no>
2018-12-17 09:40:24 +0100

    Delete Copy-Templates.ps1

--- user-management/Copy-Templates.ps1
+++
@@ -1,9 +0,0 @@
-$templateCentralStore = \\FJORD1PDC01\system$\office-templates\keolis
-$profileLoc = c:\Users\$env:UserName\Documents\Office-Templates
-
-$directoryInfo = Get-ChildItem $profileLoc | Measure-Object
-
-if ($directoryInfo.Count -eq 0)
-{
-  robocopy $templateCentralStore $profileLoc /s /r:1 /w:1
-}
